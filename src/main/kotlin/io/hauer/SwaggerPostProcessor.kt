372a2b37bfd6db66c078c8986922df65f34113f0
Jan Hauer <jan.hauer@exxeta.com>
2019-03-25 20:06:34 +0100

    Seperate swagger processor

--- src/main/kotlin/io/hauer/SwaggerPostProcessor.kt
+++ src/main/kotlin/io/hauer/SwaggerPostProcessor.kt
@@ -1,28 +1,5 @@
 package io.hauer
 
-import io.hauer.SwaggerAutoConfiguration.Companion.CONFIG_PREFIX
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
-import org.springframework.boot.context.properties.bind.Binder
-import org.springframework.core.env.Environment
 
 interface SwaggerPostProcessor : BeanFactoryPostProcessor
-
-
-/**
- * @author Jan Hauer
- * @since 1.0
- */
-class SwaggerPostProcessorImpl(private val environment: Environment, private val producer: SwaggerProducer, private val transformer: List<SwaggerTransformer>) : SwaggerPostProcessor {
-    override fun postProcessBeanFactory(factory: ConfigurableListableBeanFactory) {
-        environment.bindSwaggerConfig() //
-                .ifBound { properties ->
-                    producer.generate(properties.default, properties.groups, transformer).forEach {
-                        factory.registerSingleton(it.groupName, it)
-                    }
-                }
-    }
-
-    private fun Environment.bindSwaggerConfig() = Binder.get(this).bind(CONFIG_PREFIX, SwaggerConfig::class.java)!!
-
-}
